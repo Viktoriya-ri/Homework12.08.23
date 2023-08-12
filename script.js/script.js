cb70bbfee2b9dbb748b7c9d82336c8392fabeba8
Вікторія <viktoriya23071@gmail.com>
2023-08-12 17:38:09 +0300

    hw

---
+++ script.js/script.js
@@ -0,0 +1,93 @@
+// const url = "https://rickandmortyapi.com/api/character/";
+
+// const container = document.querySelector(".js-list");
+
+// const loadMore = document.querySelector(".js-load-more");
+// let pages = 1;
+// console.log(container);
+
+// loadMore.addEventListener("click", onLoadMore);
+
+// function servisCharacter(page=1) {
+//     return fetch(`${url}?page=${page}`).then((resp) => {
+      
+//         if (!resp.ok) {
+//             throw new Error(resp.statusText);
+//         }
+//         return resp.json();
+//     });
+
+// }
+// servisCharacter().then((data) => {
+//     console.log(data);
+//     container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+//     if (data.info.pages > 1) {
+//         loadMore.hidden = false;
+//     }
+// });
+
+// function createMarkup(arr) {
+//     return arr.map(({ image, name }) => `<li><img src="${image}" alt="${name}" width="200">
+//     <h2>${name}</h2></li>`).join("");
+// }
+
+// function onLoadMore() {
+//     pages += 1;
+//     servisCharacter(pages).then((data) => {
+//     console.log(data);
+//         container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+
+//         if (data.info.pages === pages) {
+//             loadMore.hidden = true;
+//         }
+        
+// });
+// }
+
+// !===================================================================
+
+const url = "https://rickandmortyapi.com/api/character/";
+
+const container = document.querySelector(".js-list");
+
+const loadMore = document.querySelector(".js-load-more");
+let pages = 1;
+console.log(container);
+
+loadMore.addEventListener("click", onLoadMore);
+
+function servisCharacter(page=1) {
+    return fetch(`${url}?page=${page}`).then((resp) => {
+      
+        if (!resp.ok) {
+            throw new Error(resp.statusText);
+        }
+        return resp.json();
+    });
+
+}
+servisCharacter().then((data) => {
+    console.log(data);
+    container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+    if (data.info.pages > 1) {
+        loadMore.hidden = false;
+    }
+});
+
+function createMarkup(arr) {
+    return arr.map(({ image, name }) => `<li><img src="${image}" alt="${name}" width="200">
+    <h2>${name}</h2></li>`).join("");
+}
+
+function onLoadMore() {
+    pages += 1;
+    servisCharacter(pages).then((data) => {
+    console.log(data);
+        container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+
+        if (data.info.pages === pages) {
+            loadMore.hidden = true;
+        }
+        
+});
+}
