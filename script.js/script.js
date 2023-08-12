2403a301a3f8a9c4d10cb171146f72128aa48297
Вікторія <viktoriya23071@gmail.com>
2023-08-13 00:42:51 +0300

    12.08.23

--- script.js/script.js
+++ script.js/script.js
@@ -50,11 +50,18 @@ const url = "https://rickandmortyapi.com/api/character/";
 
 const container = document.querySelector(".js-list");
 
-const loadMore = document.querySelector(".js-load-more");
 let pages = 1;
-console.log(container);
 
-loadMore.addEventListener("click", onLoadMore);
+const guard = document.querySelector(".js-guard");
+
+console.log(guard);
+
+const options = {
+  root: null,
+  rootMargin: "100px",
+};
+
+const observer = new IntersectionObserver(callback, options);
 
 function servisCharacter(page=1) {
     return fetch(`${url}?page=${page}`).then((resp) => {
@@ -66,28 +73,49 @@ function servisCharacter(page=1) {
     });
 
 }
+
+function createMarkup(arr) {
+    return arr.map(({ image, name }) => `<li><img src="${image}" alt="${name}" width="200"><h2>${name}</h2></li>`).join("");
+}
+
 servisCharacter().then((data) => {
-    console.log(data);
     container.insertAdjacentHTML("beforeend", createMarkup(data.results));
     if (data.info.pages > 1) {
-        loadMore.hidden = false;
+        observer.observe(guard);
     }
 });
 
-function createMarkup(arr) {
-    return arr.map(({ image, name }) => `<li><img src="${image}" alt="${name}" width="200">
-    <h2>${name}</h2></li>`).join("");
-}
+// function callback(entries) {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             pages += 1;
+//             servisCharacter(pages).then((data) => {
+//                 container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+//                 if (data.info.pages === pages) {
+//                     observer.unobserve(guard);
+//                 }
+//             })
+//         }
+    
+//     });
+// }
 
-function onLoadMore() {
-    pages += 1;
-    servisCharacter(pages).then((data) => {
-    console.log(data);
-        container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+// axios.get(`${url}?page=${page}`).t
 
-        if (data.info.pages === pages) {
-            loadMore.hidden = true;
+function callback(entries) {
+    entries.forEach(async (entry) => {
+        if (entry.isIntersecting) {
+            pages += 1;
+            try {
+                const data = await servisCharacter(pages);
+                container.insertAdjacentHTML("beforeend", createMarkup(data.results));
+                if (data.info.pages === pages) {
+                    observer.unobserve(guard);
+                }
+            } catch (err) {
+                console.log(err);
+     }
         }
-        
-});
+    
+    });
 }
